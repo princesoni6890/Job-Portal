ff048abe8ffe46982d02aa7321b5d1102dec9b2c
princesoni6890 <sarrafprince37@gmail.com>
2025-05-18 14:40:15 +0530

    production ready app

--- backend/index.js
+++ backend/index.js
@@ -7,24 +7,26 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config({});
+dotenv.config();
 
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+// ES module me __dirname define karna
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
-
+    origin: 'http://localhost:5173',
+    credentials: true
+};
 app.use(cors(corsOptions));
-
-const PORT = process.env.PORT || 3000 
-
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -32,9 +34,13 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+// static files for frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
